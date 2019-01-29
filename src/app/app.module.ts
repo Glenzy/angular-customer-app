96579ae7a2b77990ed852f64747e5f5c3e9b0a94
Glen Coffey <glenrcoffey@gmail.com>
2019-01-30 10:17:02 +1100

    Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersModule } from './customers/customers.module'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CustomersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import {  CustomersComponent } from './customers.component';
+import {  CustomersListComponent } from './customers-list/customers-list.component';
+
+@NgModule({
+  declarations: [CustomersComponent, CustomersListComponent,],
+  imports: [
+    CommonModule,
+  ],
+  exports: [CustomersComponent]
+})
+export class CustomersModule { 
+
+}
